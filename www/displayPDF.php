0dcf41e87cd5d87b76dba5dc424a5c4efce20657
Aaron Martin <sbbeachvball@gmail.com>
2019-02-28 16:37:09 -0800

    Adding in a settings file

--- www/displayPDF.php
+++ www/displayPDF.php
@@ -1,5 +1,7 @@
 <?php
-define('REPORTS_DB','/home/rainreports/rainreports.sqlite3');
+
+require_once('./settings.php');
+//define('REPORTS_DB','/home/rainreports/rainreports.sqlite3');
 
 require_once('./inc/rainreportsDb.class.php');
 $pdo = new SQLitePDO(REPORTS_DB);
